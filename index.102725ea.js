f6a4d1a533d54985a568a230a5d91b9af8afd6ec
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2024-04-23 14:18:48 +0300

    make build

---
+++ index.102725ea.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#thumbs"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){var r=e.target.closest(".list-item__link");e.preventDefault(),t.src=r.href});
+//# sourceMappingURL=index.102725ea.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-shadow */ "use strict";
+const list = document.querySelector("#thumbs");
+const largeImage = document.querySelector("#largeImg");
+list.addEventListener("click", (event)=>{
+    const item = event.target.closest(".list-item__link");
+    event.preventDefault();
+    largeImage.src = item.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
